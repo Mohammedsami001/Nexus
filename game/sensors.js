a33f439661626e4b928afa82464f52f95982d394
Mohammedsami001 <mohdsamisanadi@gmail.com>
2026-05-06 01:43:06 +0530

    fix: Lower the length of the sensors

--- game/sensors.js
+++ game/sensors.js
@@ -1,5 +1,5 @@
 // Raycasting to find distance to nearest obstacle
-function castProximityRay(robot, obstacles, angle, maxDistance = 500) {
+function castProximityRay(robot, obstacles, angle, maxDistance = 150) {
     const steps = 100;
     const stepSize = maxDistance / steps;
     const rad = angle * Math.PI / 180;
@@ -23,7 +23,7 @@ function castProximityRay(robot, obstacles, angle, maxDistance = 500) {
 // Cast 8 rays in a forward cone and return minimum distance
 function getProximity(robot, obstacles) {
     const angles = [-60, -40, -20, -10, 0, 10, 20, 40, 60];
-    let minDist = 500;
+    let minDist = 150;
     
     for (const offset of angles) {
         const rayAngle = robot.heading + offset;
