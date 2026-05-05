a4a1c09a4cf1ae6609b8a56ff484a48ae070e39e
Mohammedsami001 <mohdsamisanadi@gmail.com>
2026-05-06 01:55:52 +0530

    fix: reduced sensor ray length to 30 units

--- game/sensors.js
+++ game/sensors.js
@@ -23,7 +23,7 @@ function castProximityRay(robot, obstacles, angle, maxDistance = 80) {
 // Cast 8 rays in a forward cone and return minimum distance
 function getProximity(robot, obstacles) {
     const angles = [-60, -40, -20, -10, 0, 10, 20, 40, 60];
-    let minDist = 80;
+    let minDist = 30;
     
     for (const offset of angles) {
         const rayAngle = robot.heading + offset;
